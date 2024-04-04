bea1eec57c6d9a5f732b41f4e1e1249774ce8bc3
Szakalli Apri <szakaliarpi@yahoo.com>
2024-04-04 20:59:41 +0300

    -asset changes -home component changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
 								Fusing design with user goals, crafting seamless experiences.
 							</div>
 						</div>
-						<router-link class="button button--white--coral mt-30" to="/get-in-touch">Ask for an offer</router-link>
+						<router-link class="button button--white--coral mt-30" to="/get-in-touch">reach out to me</router-link>
 					</div>
 
 					<div>
@@ -37,9 +37,9 @@
 								<span class="mt-10">Software</span>
 							</router-link>
 							<router-link class="dashboard-item" to=""
-										 @mouseover="toggleHover('marketing',true)" @mouseout="toggleHover('marketing', false)">
-								<img alt="marketing" class="dashboard-item-icon" :src="isMarketingHovered ? require('../assets/dashboard/marketing-coral.svg') : require('../assets/dashboard/marketing.svg')"/>
-								<span class="mt-10">Marketing</span>
+										 @mouseover="toggleHover('data',true)" @mouseout="toggleHover('data', false)">
+								<img alt="data" class="dashboard-item-icon" :src="isDataHovered ? require('../assets/dashboard/marketing-coral.svg') : require('../assets/dashboard/marketing.svg')"/>
+								<span class="mt-10">Data</span>
 							</router-link>
 							<router-link class="dashboard-item" to=""
 										 @mouseover="toggleHover('brand',true)" @mouseout="toggleHover('brand', false)">
@@ -47,9 +47,9 @@
 								<span class="mt-10">Brand</span>
 							</router-link>
 							<router-link class="dashboard-item" to=""
-										 @mouseover="toggleHover('redesign',true)" @mouseout="toggleHover('redesign', false)">
-								<img alt="redesign" class="dashboard-item-icon" :src="isRedesignHovered ? require('../assets/dashboard/redesign-coral.svg') : require('../assets/dashboard/redesign.svg')"/>
-								<span class="mt-10">Redesign</span>
+										 @mouseover="toggleHover('rework',true)" @mouseout="toggleHover('rework', false)">
+								<img alt="rework" class="dashboard-item-icon" :src="isReworkHovered ? require('../assets/dashboard/redesign-coral.svg') : require('../assets/dashboard/redesign.svg')"/>
+								<span class="mt-10">Rework</span>
 							</router-link>
 						</div>
 					</div>
@@ -62,263 +62,110 @@
 		</div>
 
 		<div class="work-group-container">
-			<div class="work-group-item" v-on:click="setActive('ux-ui')">
-				<img alt="" class="work-icon center" src="../assets/ux-ui-design.svg"
-					 v-bind:class="{ 'work-icon-active' : isActive === 'ux-ui'}"/>
+			<div class="work-group-item" v-on:click="setActive('front-end')">
+				<img alt="" class="work-icon center" src="../assets/frontend.svg"
+					 v-bind:class="{ 'work-icon-active' : isActive === 'front-end'}"/>
 				<span class="work-group"
-					  v-bind:class="{ 'work-group-active' : isActive === 'ux-ui'}">UX/UI Design</span>
+					  v-bind:class="{ 'work-group-active' : isActive === 'front-end'}">Front-end</span>
 			</div>
 			<div class="work-group-item" v-on:click="setActive('graphic')">
-				<img alt="" class="work-icon center" src="../assets/graphic-design.svg"
+				<img alt="" class="work-icon center" src="../assets/backend.svg"
 					 v-bind:class="{ 'work-icon-active' : isActive === 'graphic'}"/>
 				<span class="work-group"
-					  v-bind:class="{ 'work-group-active': isActive === 'graphic'}">Graphic Design</span>
+					  v-bind:class="{ 'work-group-active': isActive === 'graphic'}">Back-end</span>
 			</div>
 		</div>
 
-		<div class="design-container" v-show="isActive === 'ux-ui'">
+		<div class="design-container" v-show="isActive === 'front-end'">
 			<div class="text-center mb-50">
 				<img alt="case-studies" class="case-studies " src="../assets/case-studies.svg"/>
 			</div>
 			<div>
-<!--				<div class="work-container">-->
-<!--					<img alt="flatco" class="work-container-image" src="../assets/work/bard.svg"/>-->
-<!--					<div class="work-container-text">-->
-<!--						<div class="work-container-title">Flatco</div>-->
-<!--						<div class="work-container-description">-->
-<!--							Leading owner and operator of residential real estate-->
-<!--						</div>-->
-<!--						<div>-->
-<!--							<router-link class="button button&#45;&#45;white" to="/case-study/flatco">Read case study-->
-<!--							</router-link>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--				</div>-->
-
 				<div class="work-container">
-					<img alt="hotbed" class="work-container-image" src="../assets/work/kompozit.svg"/>
+					<img alt="random1" class="work-container-image" src="../assets/randoms/random1.jpg"/>
 					<div class="work-container-text">
-						<div class="work-container-title">Kompoz.it</div>
+						<div class="work-container-title">Donec in libero</div>
 						<div class="work-container-description">
-							Field service management solution for documenting the work of service
+							Nam consequat commodo ligula, eget congue purus lacinia id.
 						</div>
 						<div>
-							<router-link class="button button--white" to="/case-study/kompozit">Read case study
+							<router-link class="button button--white" to="/case-study/kompozit">see more
 							</router-link>
 						</div>
 					</div>
 				</div>
 
 				<div class="work-container">
-					<img alt="pen" class="work-container-image" src="../assets/work/romfriend.svg"/>
+					<img alt="random2" class="work-container-image" src="../assets/randoms/random2.jpg"/>
 					<div class="work-container-text">
-						<div class="work-container-title">Romanian Friend</div>
+						<div class="work-container-title">Vivamus consectetur</div>
 						<div class="work-container-description">
-							Day trips and unique Romanian experiences
+							Aenean non purus in risus dignissim congue.
 						</div>
 						<div>
-							<router-link class="button button--white" to="/case-study/romanian-friend">Read case study</router-link>
+							<router-link class="button button--white" to="/case-study/romanian-friend">see more</router-link>
 						</div>
 					</div>
 				</div>
 
 				<div class="work-container">
-					<img alt="contractual" class="work-container-image" src="../assets/work/hotbed.svg"/>
+					<img alt="random3" class="work-container-image" src="../assets/randoms/random3.jpg"/>
 					<div class="work-container-text">
-						<div class="work-container-title">Hotbed</div>
+						<div class="work-container-title">Curabitur vulputate</div>
 						<div class="work-container-description">
-							Further’s internal software for finances, project management and statistics. My very first
-							UX and UI project.
+							Vivamus tempus tempor consequat,
 						</div>
 						<div>
-							<router-link class="button button--white" to="/case-study/hotbed">Read case study
+							<router-link class="button button--white" to="/case-study/hotbed">see more
 							</router-link>
 						</div>
 					</div>
 				</div>
-
-				<div class="work-container">
-					<img alt="pen" class="work-container-image" src="../assets/work/szulinapiujsag.svg"/>
-					<div class="work-container-text">
-						<div class="work-container-title">Szülinapi újság</div>
-						<div class="work-container-description">
-							UX and UI redesign of a page from where customers can by newspaper as a gift
-						</div>
-						<div>
-							<router-link class="button button--white" to="/case-study/szulinapi-ujsag">Read case study</router-link>
-						</div>
-					</div>
-				</div>
-
-				<div class="work-container">
-					<img alt="contractual" class="work-container-image" src="../assets/work/contractual.svg"/>
-					<div class="work-container-text">
-						<div class="work-container-title">Contractual</div>
-						<div class="work-container-description">
-							Redesigning an application that facilitates the management, creation, and storage of
-							contracts for a law firm.
-						</div>
-						<div>
-							<router-link class="button button--white" to="/case-study/contractual">Read case study
-							</router-link>
-						</div>
-					</div>
-				</div>
-
-				<!--					<div class="hide-on-desktop mt-10">-->
-				<!--						<div class="post-box">-->
-				<!--							<div class="post-title">LOW AND HIGH FIDELITY PROTOTYPES</div>-->
-				<!--							<div class="post-description">-->
-				<!--								When should you use low-fidelity prototypes? And when should you go for-->
-				<!--								high-fidelity one?-->
-				<!--								How do these both fit into user research? Find out by clicking on the read more-->
-				<!--								button.-->
-				<!--							</div>-->
-				<!--							<div class="post-bottom-container">-->
-				<!--								<div class="post-date">Nov 28th, 2022</div>-->
-				<!--								<a class="small-button button&#45;&#45;coral"-->
-				<!--								   href="https://gofurther.digital/en/blog/posts/low-and-high-fidelity-prototypes">Read-->
-				<!--									more</a>-->
-				<!--							</div>-->
-				<!--						</div>-->
-
-				<!--						<div class="post-box">-->
-				<!--							<div class="post-title">CONDUCTING USABILITY TESTS</div>-->
-				<!--							<div class="post-description">-->
-				<!--								Usability testing is a crucial step in product development.-->
-				<!--								But what happens during a usability interview and how should you facilitate such a-->
-				<!--								test?-->
-				<!--							</div>-->
-				<!--							<div class="post-bottom-container">-->
-				<!--								<div class="post-date">Sept 29th, 2022</div>-->
-				<!--								<a class="small-button button&#45;&#45;coral"-->
-				<!--								   href="https://gofurther.digital/en/blog/posts/conducting-usability-tests">Read-->
-				<!--									more</a>-->
-				<!--							</div>-->
-				<!--						</div>-->
-
-				<!--						<div class="post-box">-->
-				<!--							<div class="post-title">WHAT IS CUSTOMER JOURNEY MAPPING?</div>-->
-				<!--							<div class="post-description">-->
-				<!--								A well-designed customer journey map keeps the customer at the forefront of the-->
-				<!--								decision-making-->
-				<!--								process and highlights any struggles that customers might experience.-->
-				<!--							</div>-->
-				<!--							<div class="post-bottom-container">-->
-				<!--								<div class="post-date">Aug 25th, 2022</div>-->
-				<!--								<a class="small-button button&#45;&#45;coral"-->
-				<!--								   href="https://gofurther.digital/en/blog/posts/what-is-customer-journey-mapping">Read-->
-				<!--									more</a>-->
-				<!--							</div>-->
-				<!--						</div>-->
-
-				<!--						<div class="post-box">-->
-				<!--							<div class="post-title">HOW TO USE THE VALUE PROPOSITION CANVAS?</div>-->
-				<!--							<div class="post-description">-->
-				<!--								Why is your value proposition an important part of your business plan? How to-->
-				<!--								formulate it?-->
-				<!--								How to visualize it using the value proposition canvas?-->
-				<!--							</div>-->
-				<!--							<div class="post-bottom-container">-->
-				<!--								<div class="post-date">May 18th, 2022</div>-->
-				<!--								<a class="small-button button&#45;&#45;coral"-->
-				<!--								   href="https://gofurther.digital/en/blog/posts/how-to-use-the-value-proposition-canvas">Read-->
-				<!--									more</a>-->
-				<!--							</div>-->
-				<!--						</div>-->
-
-				<!--						<div class="post-box">-->
-				<!--							<div class="post-title">HOW TO USE PERSONAS?</div>-->
-				<!--							<div class="post-description">-->
-				<!--								What are personas, why are they important in product development and how to create-->
-				<!--								them?-->
-				<!--								Find out by reading this in-depth article about personas.-->
-				<!--							</div>-->
-				<!--							<div class="post-bottom-container">-->
-				<!--								<div class="post-date">Feb 14th, 2022</div>-->
-				<!--								<a class="small-button button&#45;&#45;coral"-->
-				<!--								   href="https://gofurther.digital/en/blog/posts/how-to-use-personas-in-product-development">Read-->
-				<!--									more</a>-->
-				<!--							</div>-->
-				<!--						</div>-->
-				<!--					</div>-->
 			</div>
 		</div>
 
-		<div class="container" v-show="isActive === 'ux-ui'">
+		<div class="container" v-show="isActive === 'front-end'">
 			<h2>Can’t get enough?</h2>
-			<span class="blog-post">Check out our blog posts co-written with Further Digital Solutions.</span>
+			<span class="blog-post">Check out some interesting posts.</span>
 
 			<div class="post-container">
 				<div class="post-box">
-					<div class="post-title">LOW AND HIGH FIDELITY PROTOTYPES</div>
+					<div class="post-title">WHERE DOES IT COME FROM?</div>
 					<div class="post-description">
-						When should you use low-fidelity prototypes? And when should you go for high-fidelity
-						one?
-						How do these both fit into user research? Find out by clicking on the read more button.
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
 					</div>
 					<div class="post-bottom-container">
-						<div class="post-date">Nov 28th, 2022</div>
+						<div class="post-date">Nov 28th, 2023</div>
 						<a class="small-button button--coral"
-						   href="https://gofurther.digital/en/blog/posts/low-and-high-fidelity-prototypes">Read
+						   href="https://www.google.com">Read more</a>
+					</div>
+				</div>
+
+				<div class="post-box">
+					<div class="post-title">WHY DO WE USE IT?</div>
+					<div class="post-description">
+						It is a long established fact that a reader will be distracted by
+						the readable content of a page when looking at its layout.
+					</div>
+					<div class="post-bottom-container">
+						<div class="post-date">Sept 20th, 2023</div>
+						<a class="small-button button--coral"
+						   href="https://www.facebook.com">Read
 							more</a>
 					</div>
 				</div>
 
 				<div class="post-box">
-					<div class="post-title">CONDUCTING USABILITY TESTS</div>
+					<div class="post-title">WHERE CAN I GET SOME?</div>
 					<div class="post-description">
-						Usability testing is a crucial step in product development.
-						But what happens during a usability interview and how should you facilitate such a test?
+						Contrary to popular belief, Lorem Ipsum is not simply random text.
+						It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
 					</div>
 					<div class="post-bottom-container">
-						<div class="post-date">Sept 29th, 2022</div>
+						<div class="post-date">Aug 25th, 2023</div>
 						<a class="small-button button--coral"
-						   href="https://gofurther.digital/en/blog/posts/conducting-usability-tests">Read
-							more</a>
-					</div>
-				</div>
-
-				<div class="post-box">
-					<div class="post-title">WHAT IS CUSTOMER JOURNEY MAPPING?</div>
-					<div class="post-description">
-						A well-designed customer journey map keeps the customer at the forefront of the
-						decision-making
-						process and highlights any struggles that customers might experience.
-					</div>
-					<div class="post-bottom-container">
-						<div class="post-date">Aug 25th, 2022</div>
-						<a class="small-button button--coral"
-						   href="https://gofurther.digital/en/blog/posts/what-is-customer-journey-mapping">Read
-							more</a>
-					</div>
-				</div>
-
-				<div class="post-box">
-					<div class="post-title">HOW TO USE THE VALUE PROPOSITION CANVAS?</div>
-					<div class="post-description">
-						Why is your value proposition an important part of your business plan? How to formulate
-						it?
-						How to visualize it using the value proposition canvas?
-					</div>
-					<div class="post-bottom-container">
-						<div class="post-date">May 18th, 2022</div>
-						<a class="small-button button--coral"
-						   href="https://gofurther.digital/en/blog/posts/how-to-use-the-value-proposition-canvas">Read
-							more</a>
-					</div>
-				</div>
-
-				<div class="post-box">
-					<div class="post-title">HOW TO USE PERSONAS?</div>
-					<div class="post-description">
-						What are personas, why are they important in product development and how to create them?
-						Find out by reading this in-depth article about personas.
-					</div>
-					<div class="post-bottom-container">
-						<div class="post-date">Feb 14th, 2022</div>
-						<a class="small-button button--coral"
-						   href="https://gofurther.digital/en/blog/posts/how-to-use-personas-in-product-development">Read
+						   href="https://www.instagram.com">Read
 							more</a>
 					</div>
 				</div>
@@ -345,9 +192,7 @@
 			</div>
 
 		</div>
-
 		<Contact></Contact>
-
 	</div>
 </template>
 
@@ -364,13 +209,13 @@ export default defineComponent({
 	},
 	data() {
 		return {
-			isActive: 'ux-ui',
+			isActive: 'front-end',
 			isWebHovered: false,
 			isPhoneHovered: false,
 			isSoftwareHovered: false,
-			isMarketingHovered: false,
+			isDataHovered: false,
 			isBrandHovered: false,
-			isRedesignHovered: false,
+			isReworkHovered: false,
 		};
 	},
 	methods: {
@@ -392,14 +237,14 @@ export default defineComponent({
 				case 'software':
 					this.isSoftwareHovered = value;
 					break;
-				case 'marketing':
-					this.isMarketingHovered = value;
+				case 'data':
+					this.isDataHovered = value;
 					break;
 				case 'brand':
 					this.isBrandHovered = value;
 					break;
-				case 'redesign':
-					this.isRedesignHovered = value;
+				case 'rework':
+					this.isReworkHovered = value;
 					break;
 				default:
 					break;
